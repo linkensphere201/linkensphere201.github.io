948c4ec34d01721046b9a92286c9d570f473032c
fatiao <me2019@aliyun.com>
2020-11-07 22:20:10 +0800

    hugo publish - 2020-11-07-22:20:10

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-ts=$(date "+%Y-%m-%d-%H:%M:%S"); 
-msg="hugo publish - $ts";
-git add .;
+ts=$(date "+%Y-%m-%d-%H:%M:%S")
+msg="hugo publish - $ts"
+work_path=$(dirname `readlink -f $0`)
+repo_path=${work_path}/../
+
+cd ${repo_path}
+git add .
 filechanged=$(git status);
 echo "publish msg: $msg";
 echo "file changed: $filechanged";
 git commit -m "$msg";
 git push origin main
-
+cd -
