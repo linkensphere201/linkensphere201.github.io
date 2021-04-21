73e71a3911538134eca358cf7d7287d4954d19b2
fatiao <me2019@aliyun.com>
2021-04-21 13:59:11 +0800

    hugo publish - 2021-04-21-13:59:10

--- js/onload.js
+++ js/onload.js
@@ -1,3 +1,20 @@
+function GetRandomWall(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "47.99.216.43:18080/bannerback", true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function(){
+        var XMLHttpReq = xhr;
+        if (XMLHttpReq.readyState == 4) {
+            if (XMLHttpReq.status == 200) {                
+                var data = XMLHttpReq.responseText;
+                console.log(data);
+                document.getElementById('bannerback').style.backgroundImage = "url("+ data +")";
+            }
+        }
+    };
+    xhr.send();
+}
 function onLoad() {
   console.log('loaded!');
+  // GetRandomWall()
 }
